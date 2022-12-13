97ff08e16ab078745c99666b8e7615ee3b311d1f
kaceykniffen <kaceykniffen@gmail.com>
2022-12-12 19:28:41 -0600

    Added Javascript Functionality

--- src/components/PricingMain.tsx
+++ src/components/PricingMain.tsx
@@ -1,26 +1,83 @@
+import { ChangeEvent, useEffect, useState } from "react";
 import "./Pricing.scss";
 
+const PAGE_VIEWS = ["10k", "50k", "100k", "500k", "1m"];
+const PRICE_MONTHLY = [8, 12, 16, 24, 36];
+
+function Switch({ toggleFunc }: { toggleFunc: Function }) {
+	const [checked, setChecked] = useState(false);
+	const handleChange = () => {
+		setChecked(!checked);
+	};
+
+	useEffect(() => {
+		toggleFunc(checked);
+	}, [checked]);
+
+	return (
+		<div className="paymentswitch">
+			<label htmlFor="switch">Monthly Billing</label>
+			<label className="switch" id="switch">
+				<input type="checkbox" onChange={handleChange} />
+				<span className="slider round"></span>
+			</label>
+			<label className="yearly">
+				Yearly Billing <span className="discount">25% discount</span>
+			</label>
+		</div>
+	);
+}
+
 export default function PricingComponent() {
+	const [isYearly, setIsYearly] = useState(true);
+	const [sliderValue, setSliderValue] = useState(0);
+
+	const [pageViews, setPageViews] = useState(PAGE_VIEWS[sliderValue]);
+	const [price, setPrice] = useState(PRICE_MONTHLY[sliderValue]);
+
+	const toggleIsYearly = (newVal: boolean) => {
+		setIsYearly(() => {
+			return newVal;
+		});
+	};
+
+	const sliderChange = (e: ChangeEvent) => {
+		const target = e.target as HTMLInputElement;
+		setSliderValue(parseFloat(target.value));
+	};
+
+	useEffect(() => {
+		setPageViews(() => {
+			return PAGE_VIEWS[sliderValue];
+		});
+	}, [sliderValue]);
+
+	useEffect(() => {
+		setPrice(() => {
+			if (isYearly)
+				return PRICE_MONTHLY[sliderValue] - PRICE_MONTHLY[sliderValue] * 0.25;
+			return PRICE_MONTHLY[sliderValue];
+		});
+	}, [sliderValue, isYearly]);
+
 	return (
 		<section className="pricing">
 			<div className="top">
 				<div className="pricesection">
-					<div className="pageviews">100k pageviews</div>
+					<div className="pageviews">{pageViews} pageviews</div>
 					<div className="price">
-						$16.00 <span className="per">/month</span>
+						${price}.00{" "}
+						<span className="per">/{isYearly ? "year" : "month"}</span>
 					</div>
 				</div>
-				<input type="range" min={0} max={6} />
-				<div className="paymentswitch">
-					<label htmlFor="switch">Monthly Billing</label>
-					<label className="switch" id="switch">
-						<input type="checkbox" />
-						<span className="slider round"></span>
-					</label>
-					<label>
-						Yearly Billing <span className="discount">25% discount</span>
-					</label>
-				</div>
+				<input
+					onChange={sliderChange}
+					value={sliderValue}
+					type="range"
+					min={0}
+					max={4}
+				/>
+				<Switch toggleFunc={toggleIsYearly} />
 			</div>
 			<hr />
 			<div className="bottom">
